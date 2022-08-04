699efe5a3386ea0b379b8fa94e37166663092d65
Refilwe-M-Mashile <refilwemerriam.mashile@vcontractor.co.za>
2022-08-04 19:14:57 +0200

    naming variables

--- stringChallenge.js
+++ stringChallenge.js
@@ -1,21 +1,22 @@
 const allAboutStrings = (str) => {
   const output = [];
   const strLength = str.length;
+
   output.push(strLength); //length of string
   output.push(str[0]); //first character
   output.push(str[strLength - 1]); //last character
 
-  const half = Math.floor(strLength / 2);
+  const middleIndex = Math.floor(strLength / 2);
   /* check if the length is odd or even then push the middle character(s)*/
   strLength % 2 === 0
-    ? output.push(str[half - 1] + str[half])
-    : output.push(str[half]);
+    ? output.push(str[middleIndex - 1] + str[middleIndex])
+    : output.push(str[middleIndex]);
 
-  const secCharacter = str[1]; //second Character
-  const moreChar = str.indexOf(secCharacter, 2);
+  const secondCharacter = str[1]; //second Character
+  const secondOccurrence = str.indexOf(secondCharacter, 2);
   //check another appearance of the second character
-  moreChar !== -1
-    ? output.push(`@ index ${moreChar}`)
+  secondOccurrence !== -1
+    ? output.push(`@ index ${secondOccurrence}`)
     : output.push("not found");
 
   return output;
